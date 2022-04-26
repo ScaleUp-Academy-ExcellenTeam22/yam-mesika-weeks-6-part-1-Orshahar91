951acfcc8ef3a18feecdbb83c5984fe73ac19ab3
Or Shahar <orshahar91@gmail.com>
2022-04-26 14:30:44 +0300

    Code review - get_letters

--- get_letters.py
+++ get_letters.py
@@ -1,5 +1,7 @@
-# returns the entire alphabet - lower case and upper case
 def get_letters():
+    """
+    :return: # The entire alphabet - lower case and upper case.
+    """
     return [chr(lowerCase_letter) for lowerCase_letter in range(ord('a'), ord('z') + 1)] +\
            [chr(upperCase_letter) for upperCase_letter in range(ord('A'), ord('Z') + 1)]
 
