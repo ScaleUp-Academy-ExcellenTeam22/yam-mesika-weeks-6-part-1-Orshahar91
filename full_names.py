f8adbe70b556e7d558ddb3ceddb8cd5714a97acf
Or Shahar <orshahar91@gmail.com>
2022-04-26 14:22:17 +0300

    code review - full_names

--- full_names.py
+++ full_names.py
@@ -1,6 +1,11 @@
-# returns all the different combinations of first name and last name
-# with the minimum length provided
 def full_names(first_names, last_names, min_length=1):
+    """
+    :param list first_names: list of strings containing first names.
+    :param list last_names: list of strings containing last names.
+    :param int min_length: minimum string length for first name + last name
+    :return: All the different combinations of first name and last name
+             with the minimum length provided
+    """
     full_names_variations = [first_name.capitalize() + " " + last_name.capitalize() for first_name in first_names
                              for last_name in last_names if len(first_name) + len(last_name) + 1 >= int(min_length)]
     return full_names_variations
